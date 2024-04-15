a47e14d0ab3162b865db289dae8812930e6f6e8e
Pauline123456789 <novoseltseva.polina12@bk.ru>
2024-04-15 22:26:13 +0700

    add fun2

--- main.pyi
+++ main.pyi
@@ -1,12 +1,14 @@
 def add(a, b):
     return a + b
 
-
+def subtract(a, b):
+    return a - b
      
 
 def display_menu():
     print("Выберите операцию:")
     print("1. Сложение")
+    print("2. Вычитание")
     
     print("0. Выход")
 
@@ -21,6 +23,9 @@ def main():
         elif choice == '1':
             num1, num2 = map(float, input("Введите два числа через пробел: ").split())
             print("Результат сложения:", add(num1, num2))
+        elif choice == '2':
+            num1, num2 = map(float, input("Введите два числа через пробел: ").split())
+            print("Результат вычитания:", subtract(num1, num2))
         
         else:
             print("Неверный ввод. Попробуйте снова.")
